a6b6cd0fb28d567a9bddeb2a666395a941823752
Shubham Pawar <shubhampawar3007@gmail.com>
2021-12-24 17:34:03 +0530

    removed default vite react code

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = function () {
+  return (
+    <div>
+      <h1 className="text-4xl text-center underline text-pink-700">Hello from react</h1>
+    </div>
+  );
+};
+
+export default App;

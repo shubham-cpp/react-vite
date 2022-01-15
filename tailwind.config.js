4900a22432deec798bfa08d3ab182534673e6911
Shubham Pawar <shubhampawar3007@gmail.com>
2022-01-15 10:11:26 +0530

    added fonts and container options(for tailwindcss)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      serif: ['IBM Plex Serif', ...defaultTheme.fontFamily.serif],
+      mono: ['IBM Plex Mono', ...defaultTheme.fontFamily.mono],
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
   },
   plugins: [],
 };
